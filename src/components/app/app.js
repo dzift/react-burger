6aeea6caf74df9601c0962e9a9b56a43c8b1e45b
dzift <dvkubist@gmail.com>
2022-04-17 15:57:06 +1000

    add Header

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -13,21 +13,21 @@ function App() {
       <header className="appHeader">
         <div className="contentHeader">
           <div className="menu">
-            <a href="/" className="itemHeader">
+            <a href="/" className="itemHeader pl-5 pr-5 mr-2 mt-4 mb-4">
               <BurgerIcon type="primary" />
-              Конструктор
+              <span className="ml-2">Конструктор</span>
             </a>
-            <a href="/" className="itemHeader">
+            <a href="/" className="itemHeader pl-5 pr-5 mt-4 mb-4">
               <ListIcon type="secondary" />
-              Лента заказов
+              <span className="ml-2">Лента заказов</span>
             </a>
           </div>
           <a href="/" className="itemHeader">
             <Logo />
           </a>
-          <a href="/" className="itemHeader">
+          <a href="/" className="itemHeader  mt-4 mb-4">
             <ProfileIcon type="secondary" />
-            Личный кабинет
+            <span className="ml-2">Личный кабинет</span>
           </a>
         </div>
       </header>
