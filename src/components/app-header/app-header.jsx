c75c94135772da20f928f0725326cf4ab9b18c3e
dzift <dvkubist@gmail.com>
2022-06-21 18:18:49 +1000

    добавил страницу профиля, сделал на ней роутинг,сделала активные nanlink

--- src/components/app-header/app-header.jsx
+++ src/components/app-header/app-header.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./app-header.module.css";
+import { NavLink } from "react-router-dom";
 
 import {
   BurgerIcon,
@@ -13,28 +14,37 @@ const AppHeader = () => {
     <header className={styles.appHeader}>
       <div className={styles.contentHeader}>
         <div className={styles.menu}>
-          <a
-            href="/"
-            className={`${styles.active} pl-5 pr-5 mr-2 mt-4 mb-4 text  `}
+          <NavLink
+            to="/"
+            exact
+            activeClassName={styles.link_active}
+            className={`${styles.link} pl-5 pr-5 mr-2 mt-4 mb-4 `}
           >
             <BurgerIcon type="primary" />
             <span className="ml-2">Конструктор</span>
-          </a>
-          <a
-            href="/"
-            className={`pl-5 pr-5 mt-4 mb-4 text text_color_inactive`}
+          </NavLink>
+          <NavLink
+            to="/orders"
+            exact
+            activeClassName={styles.link_active}
+            className={` ${styles.link} pl-5 pr-5 mt-4 mb-4 `}
           >
             <ListIcon type="secondary" />
             <span className="ml-2">Лента заказов</span>
-          </a>
+          </NavLink>
         </div>
-        <a href="/" className={styles.logo}>
+        <NavLink to="/" className={styles.logo}>
           <Logo />
-        </a>
-        <a href="/" className="mt-4 mb-4 pr-5 text text_color_inactive">
+        </NavLink>
+        <NavLink
+          to="/profile"
+          exact
+          activeClassName={styles.link_active}
+          className={`${styles.link} mt-4 mb-4 pr-5 `}
+        >
           <ProfileIcon type="secondary" />
           <span className="ml-2">Личный кабинет</span>
-        </a>
+        </NavLink>
       </div>
     </header>
   );
