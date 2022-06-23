bcf2de3cfc3f1af9c7432caaa51777599d2f519b
dzift <dvkubist@gmail.com>
2022-06-23 15:37:01 +1000

    добавил ProtectedRoute

---
+++ src/components/protected-route/protected-route.jsx
@@ -0,0 +1,9 @@
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import Preloader from "./preloader/preloader";
+
+export const ProtectedRoute = () => {
+  const dispatch = useDispatch();
+
+  return <div></div>;
+};
